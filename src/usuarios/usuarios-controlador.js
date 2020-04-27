5ede6dda2b87360f2707e3003c5f46d85492acc9
Andrew Ijano <andrew.lopes@uol.com.br>
2020-04-27 18:13:00 -0300

    Add first implementation for refresh tokens with failing verification

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -3,6 +3,7 @@ const { InvalidArgumentError, InternalServerError } = require('../erros');
 
 const jwt = require('jsonwebtoken');
 const blacklist = require('../../redis/manipula-blacklist');
+const crypto = require('crypto');
 
 function criaTokenJWT(usuario) {
   const payload = {
@@ -13,32 +14,63 @@ function criaTokenJWT(usuario) {
   return token;
 }
 
-async function encripta(payload, chave) {
+function encripta(payload, chave) {
+  chave = crypto.scryptSync(chave, 'salt', 32);
+  console.log(chave);
+  // iv: initialization vector
+  const vetorInicializacao = crypto.randomBytes(16);
+  const encriptador = crypto.createCipheriv(
+    'aes-256-gcm',
+    chave,
+    vetorInicializacao
+  );
+  const payloadEncriptado =
+    encriptador.update(payload, 'utf8') + encriptador.final();
+  const tag = encriptador.getAuthTag();
+  const refreshToken = JSON.stringify({
+    payloadEncriptado,
+    tag,
+    vetorInicializacao
+  });
+  return Buffer.from(refreshToken).toString('base64');
+}
+
+function decripta(token, chave) {
+  chave = crypto.scryptSync(chave, 'salt', 32);
+  console.log(chave);
+  const { payloadEncriptado, tag, vetorInicializacao } = JSON.parse(
+    Buffer.from(token, 'base64')
+  );
+  const decriptador = crypto.createDecipheriv(
+    'aes-256-gcm',
+    chave,
+    Buffer.from(vetorInicializacao)
+  );
+  decriptador.setAuthTag(Buffer.from(tag));
+
+  const payload = decriptador.update(payloadEncriptado) + decriptador.final();
+
   return payload;
 }
 
-async function criaRefreshToken(usuario) {
+function criaRefreshToken(usuario) {
   const chave = 'senha-secreta';
   const nonce = 'aaaaa';
-  const payload = {
+  const payload = JSON.stringify({
     id: usuario.id,
-    exp: Date.now() + 1000*60*60*24*5,
+    exp: Date.now() + 1000 * 60 * 60 * 24 * 5,
     nonce
-  }
+  });
 
-  const refreshToken = await encripta(payload, chave);
+  const refreshToken = encripta(payload, chave);
   return refreshToken;
 }
 
-async function decripta(token) {
-  return token;
-}
-
-async function verificaRefreshToken(token) {
-  const payload = await decripta(token);
+function verificaRefreshToken(token) {
+  const chave = 'senha-secreta';
+  const payload = decripta(token, chave);
   return payload;
 }
-
 
 module.exports = {
   async adiciona(req, res) {
@@ -66,10 +98,15 @@ module.exports = {
     }
   },
 
-  login(req, res) {
-    const token = criaTokenJWT(req.user);
-    res.set('Authorization', token);
-    res.status(204).json();
+  async login(req, res) {
+    try {
+      const accessToken = criaTokenJWT(req.user);
+      const refreshToken = criaRefreshToken(req.user);
+      res.set('Authorization', accessToken);
+      res.status(200).json({ refreshToken: refreshToken });
+    } catch (erro) {
+      res.status(500).json({ erro: erro.message });
+    }
   },
 
   async logout(req, res) {
