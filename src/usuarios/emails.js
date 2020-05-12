2e8180d1d08d79977d7abb37a6893764a2092a9a
Andrew Ijano <andrew.lopes@uol.com.br>
2020-05-12 18:35:14 -0300

    Add production configuration to email sending

--- src/usuarios/emails.js
+++ src/usuarios/emails.js
@@ -1,17 +1,41 @@
 const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 
+class ConfiguracaoEmail {
+  constructor() {
+    this.host = process.env.EMAIL_HOST;
+    this.auth = {
+      user: process.env.EMAIL_USUARIO,
+      pass: process.env.EMAIL_SENHA
+    };
+  }
+}
+
+class ConfiguracaoEmailTeste {
+  constructor(contaTeste) {
+    this.host = 'smtp.ethereal.email';
+    this.auth = contaTeste;
+  }
+}
+
+async function criaConfiguracaoEmail() {
+  if (process.env.NODE_ENV === 'production') {
+    return new ConfiguracaoEmail();
+  } else {
+    const contaTeste = await nodemailer.createTestAccount();
+    return new ConfiguracaoEmailTeste(contaTeste);
+  }
+}
+
 class Email {
   async enviaEmail() {
-    const contaTeste = await nodemailer.createTestAccount();
-
-    const transportador = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      auth: contaTeste
-    });
-
+    const configuracaoEmail = await criaConfiguracaoEmail();
+    const transportador = nodemailer.createTransport(configuracaoEmail);
     const info = await transportador.sendMail(this);
-    console.log('URL:' + nodemailer.getTestMessageUrl(info));
+    // Tem como mandar imprimir isso apenas em desenvolvimento sem usar if?
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('URL:' + nodemailer.getTestMessageUrl(info));
+    }
   }
 }
 
