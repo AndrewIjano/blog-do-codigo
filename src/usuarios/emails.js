8dc24ff635dc93a240caac6c0d77db695d811447
Andrew Ijano <andrew.ijano@gmail.com>
2020-06-12 15:42:01 -0300

    Refactors to production

--- src/usuarios/emails.js
+++ src/usuarios/emails.js
@@ -1,15 +1,41 @@
 const nodemailer = require('nodemailer');
 
+class ConfiguracaoEmail {
+  constructor() {
+    this.host = process.env.EMAIL_HOST;
+    this.auth = {
+      user: process.env.EMAIL_USUARIO,
+      pass: process.env.EMAIL_SENHA
+    };
+    this.secure = true;
+  }
+}
+
+class ConfiguracaoEmailTeste {
+  constructor(contaTeste) {
+    this.host = 'smtp.ethereal.email';
+    this.auth = contaTeste;
+  }
+}
+
+async function criaConfiguracaoEmail() {
+  if (process.env.NODE_ENV === 'production') {
+    return new ConfiguracaoEmail();
+  } else {
+    const contaTeste = await nodemailer.createTestAccount();
+    return new ConfiguracaoEmailTeste(contaTeste);
+  }
+}
+
 class Email {
   async enviaEmail() {
-    const contaTeste = await nodemailer.createTestAccount();
-    const transportador = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      auth: contaTeste
-    });
+    const configuracaoEmail = await criaConfiguracaoEmail();
+    const transportador = nodemailer.createTransport(configuracaoEmail);
     const info = await transportador.sendMail(this);
-  
-    console.log('URL:' + nodemailer.getTestMessageUrl(info));
+
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('URL:' + nodemailer.getTestMessageUrl(info));
+    }
   }
 }
 
